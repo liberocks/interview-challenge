06f1423190887d3443265314f04d6ae6406636b3
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2025-07-13 10:42:33 +0700

    chores: remove unused sample module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
-import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
 
-import { SampleModule } from './domain/sample/sample.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './domain/patient/patient.module';
@@ -16,7 +15,7 @@ import { AssignmentModule } from './domain/assignment/assignment.module';
       entities: [`${__dirname}/**/*.entity{.ts,.js}`],
       synchronize: true,
     }),
-    SampleModule,
+
     PatientModule,
     MedicationModule,
     AssignmentModule,
